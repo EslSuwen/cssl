49f4b3c86630faa4de9356e6fe705d26ea74899a
wangyang-o <1370228031@qq.com>
2020-07-15 20:49:33 +0800

    修改

--- src/main/webapp/LabAdmin2.0/src/app/side-nav/show-notice/show-notice.component.ts
+++ src/main/webapp/LabAdmin2.0/src/app/side-nav/show-notice/show-notice.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./show-notice.component.scss"],
 })
 export class ShowNoticeComponent implements OnInit {
-  content='<blockquote>测试</blockquote><blockquote>wsqfcaq</blockquote><h2>fdgdbfjg</h2><p><span style="color: rgb(230, 0, 0);">ygjuhljnlkk</span></p><p><span style="color: rgb(230, 0, 0);">ffhhhrstjuykd</span></p><p><span style="color: rgb(230, 0, 0); background-color: rgb(0, 71, 178);">gjhkhjolihpilhijl</span></p>';
+  content='<blockquote>富文本测试</blockquote><blockquote>wsqfcaq</blockquote><h2>fdgdbfjg</h2><p><span style="color: rgb(230, 0, 0);">ygjuhljnlkk</span></p><p><span style="color: rgb(230, 0, 0);">ffhhhrstjuykd</span></p><p><span style="color: rgb(230, 0, 0); background-color: rgb(0, 71, 178);">gjhkhjolihpilhijl</span></p>';
   constructor() {}
 
   ngOnInit() {}
